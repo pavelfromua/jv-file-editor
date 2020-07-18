31cf00c71291139e8a1201ef73c1084a533391f9
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-07-18 17:57:08 +0300

    errors from travis are fixed

--- src/main/java/FileEditor.java
+++ src/main/java/FileEditor.java
@@ -2,7 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
 
